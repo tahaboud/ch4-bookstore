7bb0b0d74206789114d3c05bcc9ab1cb07d92012
Boudouaoui Taha <tahaboudouaoui@gmail.com>
2022-12-07 18:27:28 +0100

    ch6

--- django_project/urls.py
+++ django_project/urls.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # Django Admin
     path('admin/', admin.site.urls),
+    # User Management
+    path("accounts/", include("django.contrib.auth.urls")),
+    # Local Apps
+    path("accounts/", include("accounts.urls")),
     path("", include("pages.urls")),
 ]
